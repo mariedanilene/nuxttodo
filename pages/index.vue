dda99ef2cfc3e101d655f521e582b5806d3c34e3
Marie Danilene <bulosan.mariedanilene@gmail.com>
2019-05-17 22:30:27 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="ui one column grid container">
     <div class="column">
+      <h2 class="ui header">Todo List</h2>
+    </div>
+    <div class="column">
       <task-form/>
     </div>
     <div class="column">
